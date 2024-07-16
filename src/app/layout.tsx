23f05dcd474bc610f3499d0acc5f754b47cc390e
web-luisaguilar <web.luisaguilar@gmail.com>
2024-07-16 11:35:25 -0600

    feat: google analitycs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,31 @@
-import { Montserrat } from 'next/font/google'
-import '@/styles/globals.css'
-import '@/styles/Holi.css'
-import Header from '@/components/Header'
-import BackToTop from '@/components/BackToTop'
-import Footer from '@/components/Footer'
-import StoreProvider from './StoreProvider'
+import { Montserrat } from "next/font/google";
+import "@/styles/globals.css";
+import "@/styles/Holi.css";
+import Header from "@/components/Header";
+import BackToTop from "@/components/BackToTop";
+import Footer from "@/components/Footer";
+import StoreProvider from "./StoreProvider";
 
-const montserrat = Montserrat({ subsets: ['latin'] })
+const montserrat = Montserrat({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Luis Aguilar ',
+  title: "Luis Aguilar ",
   description:
-    'Portafolio web de Luis Aguilar donde encontraras diferentes proyectos desarrollados por mi con el uso de distintas tecnologias. Espero que encuentres lo que buscas',
-}
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+    "Portafolio web de Luis Aguilar donde encontraras diferentes proyectos desarrollados por mi con el uso de distintas tecnologias. Espero que encuentres lo que buscas",
+};
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="es">
+      <head>
+        <meta
+          name="google-site-verification"
+          content="f5EiQcKq1E4yi0c20Wsx81vXH3dZ4QbmsK04I1DfybM"
+        />
+      </head>
       <body className={montserrat.className}>
         <Header />
         <StoreProvider>{children}</StoreProvider>
@@ -23,5 +33,5 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <Footer />
       </body>
     </html>
-  )
+  );
 }
