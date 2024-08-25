5ed5f5a29b278ba50218747d592b6a292405e905
LuisAguilar <web.luisaguilar@gmail.com>
2024-08-25 12:59:29 -0600

    feat: added new contact method (email)

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,7 +2,7 @@ import ContactForm from '@/components/contact/ContactForm'
 import SocialMedias from '@/components/contact/SocialMedias'
 
 export const metadata = {
-  title: 'Contacto | LuisAguilar',
+  title: 'Contacto | LuisAguilar'
 }
 const ContactPage = () => {
   return (
@@ -12,6 +12,12 @@ const ContactPage = () => {
       <br />
       <h2 className="text-center">OTROS METODOS DE CONTACTO</h2>
       <SocialMedias />
+      <h2 className="text-center pb-4"> ✉️ Enviame un correo </h2>
+      <h3 className="text-center text-white ">
+        <span className="bg-customTheme-950/50 px-8 py-4 rounded-md hover:border-customTheme-900/80 hover:border">
+          web.luisaguilar@gmail.com
+        </span>
+      </h3>
     </main>
   )
 }
