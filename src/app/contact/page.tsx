93d061ff23a675c67dc00c97eab764228958aca5
web-luisaguilar <web.luisaguilar@gmail.com>
2025-05-30 23:02:40 -0600

    contact section position change

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -7,17 +7,21 @@ export const metadata = {
 const ContactPage = () => {
   return (
     <main>
-      <h1 className="text-center">Formulario de contacto</h1>
-      <ContactForm />
+      <section className="py-40">
+        <h1 className="text-center pb-4"> ✉️ Enviame un correo </h1>
+
+        <h2 className="text-center text-white ">
+          <span className="bg-customTheme-950/50 px-8 py-4 rounded-md hover:border-customTheme-900/80 hover:border">
+            web.luisaguilar@gmail.com
+          </span>
+        </h2>
+      </section>
+
       <br />
       <h2 className="text-center">OTROS METODOS DE CONTACTO</h2>
       <SocialMedias />
-      <h2 className="text-center pb-4"> ✉️ Enviame un correo </h2>
-      <h3 className="text-center text-white ">
-        <span className="bg-customTheme-950/50 px-8 py-4 rounded-md hover:border-customTheme-900/80 hover:border">
-          web.luisaguilar@gmail.com
-        </span>
-      </h3>
+      <h2 className="text-center">Formulario de contacto</h2>
+      <ContactForm />
     </main>
   )
 }
